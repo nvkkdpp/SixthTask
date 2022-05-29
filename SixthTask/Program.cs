5c749cc80074efc66662c2410cfd4da6245439e5
nvkkdpp <niggamemes@ya.ru>
2022-05-30 02:22:45 +0900

    commit

---
+++ SixthTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace SixthTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] prostranstvo = { ' ', '\n', '\t' };
+            string s = " this is a \nzhopa";
+            Console.WriteLine("До" + s);
+            Console.Write("После ");
+            for (; ; )
+            {
+                int offset = s.IndexOfAny(prostranstvo);
+                if (offset == -1)
+                {
+                    break;
+                }
+                string before = s.Substring(0, offset);
+                string after = s.Substring(offset + 1);
+
+                s = string.Concat(before, after);
+            }
+            Console.WriteLine(s);
+
+            Console.WriteLine("Haжмитe <Enter> для завершения программы ... ");
+
+            Console.Read();
+        }
+
+    }
+}
+
+
